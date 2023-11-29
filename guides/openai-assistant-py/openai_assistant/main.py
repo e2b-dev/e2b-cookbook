7fd8b56a526d557e4ebeb242363be68939d02092
Tereza Tizkova <tereza@e2b.dev>
2023-11-29 12:56:59 +0100

    Update OpenAI guide

--- guides/openai-assistant-py/openai_assistant/main.py
+++ guides/openai-assistant-py/openai_assistant/main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from e2b import Sandbox
 import openai
+import time
 
 from actions import read_file, save_code_to_file, list_files
 
@@ -61,6 +62,7 @@ def main():
             break
 
         run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+        time.sleep(0.2)
 
     sandbox.close()
 

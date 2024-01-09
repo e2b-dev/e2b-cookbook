b6461532bb6e63352c7d98c4009c03b3c4c1b7ca
Jakub Novak <jakub@e2b.dev>
2024-01-09 18:03:21 +0100

    Create llama base template

---
+++ templates/e2b-llama-index/backend/app/api/routers/chat.py
@@ -0,0 +1,62 @@
+from typing import List
+
+from fastapi.responses import StreamingResponse
+from llama_index.chat_engine.types import BaseChatEngine
+
+from app.engine.index import get_chat_engine
+from fastapi import APIRouter, Depends, HTTPException, Request, status
+from llama_index.llms.base import ChatMessage
+from llama_index.llms.types import MessageRole
+from pydantic import BaseModel
+
+chat_router = r = APIRouter()
+
+
+class _Message(BaseModel):
+    role: MessageRole
+    content: str
+
+
+class _ChatData(BaseModel):
+    messages: List[_Message]
+
+
+@r.post("")
+async def chat(
+    request: Request,
+    data: _ChatData,
+    chat_engine: BaseChatEngine = Depends(get_chat_engine),
+):
+    # check preconditions and get last message
+    if len(data.messages) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="No messages provided",
+        )
+    last_message = data.messages.pop()
+    if last_message.role != MessageRole.USER:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Last message must be from user",
+        )
+    # convert messages coming from the request to type ChatMessage
+    messages = [
+        ChatMessage(
+            role=m.role,
+            content=m.content,
+        )
+        for m in data.messages
+    ]
+
+    # query chat engine
+    response = chat_engine.stream_chat(last_message.content, messages)
+
+    # stream response
+    async def event_generator():
+        for token in response.response_gen:
+            # If client closes connection, stop sending events
+            if await request.is_disconnected():
+                break
+            yield token
+
+    return StreamingResponse(event_generator(), media_type="text/plain")

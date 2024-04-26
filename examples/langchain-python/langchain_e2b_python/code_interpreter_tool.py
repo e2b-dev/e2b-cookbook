bea6f6c6bef8117a680f23ceec16228527f6b4f6
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-04-26 12:43:21 -0700

    Update code interpreter tool in langchain integration

--- examples/langchain-python/langchain_e2b_python/code_interpreter_tool.py
+++ examples/langchain-python/langchain_e2b_python/code_interpreter_tool.py
@@ -1,53 +1,80 @@
 import os
-from typing import Any
+import json
 
-from langchain_core.tools import tool, Tool
+from typing import Any, List
+from langchain_core.tools import Tool
 from pydantic.v1 import BaseModel, Field
 from e2b_code_interpreter import CodeInterpreter
+from langchain_core.messages import BaseMessage, ToolMessage
+from langchain.agents.output_parsers.tools import (
+    ToolAgentAction,
+)
+
 
 class LangchainCodeInterpreterToolInput(BaseModel):
-  code: str = Field(description="Python code to execute.")
+    code: str = Field(description="Python code to execute.")
 
 
 class CodeInterpreterFunctionTool:
-  """
-  This class calls arbitrary code against a Python Jupyter notebook.
-  It requires an E2B_API_KEY to create a sandbox.
-  """
+    """
+    This class calls arbitrary code against a Python Jupyter notebook.
+    It requires an E2B_API_KEY to create a sandbox.
+    """
 
-  def __init__(self):
-    # Instantiate the E2B sandbox - this is a long lived object
-    # that's pinging E2B cloud to keep the sandbox alive.
-    if "E2B_API_KEY" not in os.environ:
-      raise Exception(
-        "Code Interpreter tool called while E2B_API_KEY environment variable is not set. Please get your E2B api key here https://e2b.dev/docs and set the E2B_API_KEY environment variable."
-      )
-    self.code_interpreter = CodeInterpreter()
+    tool_name: str = "code_interpreter"
 
-  def call(self, parameters: dict, **kwargs: Any):
-    # TODO: E2B supports generating and streaming charts and other rich data
-    # because it has a full Jupyter server running inside the sandbox.
-    # What's the best way to send this data back to frontend and render them in chat?
+    def __init__(self):
+        # Instantiate the E2B sandbox - this is a long lived object
+        # that's pinging E2B cloud to keep the sandbox alive.
+        if "E2B_API_KEY" not in os.environ:
+            raise Exception(
+                "Code Interpreter tool called while E2B_API_KEY environment variable is not set. Please get your E2B api key here https://e2b.dev/docs and set the E2B_API_KEY environment variable."
+            )
+        self.code_interpreter = CodeInterpreter()
 
-    code = parameters.get("code", "")
-    print(f"️✴️✴️✴️Code Interpreting...\n{code}\n====")
-    execution = self.code_interpreter.notebook.exec_cell(code)
-    return {
-      "results": execution.results,
-      "stdout": execution.logs.stdout,
-      "stderr": execution.logs.stderr,
-      "error": execution.error,
-    }
+    def call(self, parameters: dict, **kwargs: Any):
+        # TODO: E2B supports generating and streaming charts and other rich data
+        # because it has a full Jupyter server running inside the sandbox.
+        # What's the best way to send this data back to frontend and render them in chat?
 
-  # langchain does not return a dict as a parameter, only a code string
-  def langchain_call(self, code: str):
-    return self.call({"code": code})
+        code = parameters.get("code", "")
+        print(f"***Code Interpreting...\n{code}\n====")
+        execution = self.code_interpreter.notebook.exec_cell(code)
+        return {
+            "results": execution.results,
+            "stdout": execution.logs.stdout,
+            "stderr": execution.logs.stderr,
+            "error": execution.error,
+        }
 
-  def to_langchain_tool(self) -> Tool:
-    tool = Tool(
-      name="code_interpreter",
-      description="Execute python code in a Jupyter notebook cell and returns any rich data (eg charts), stdout, stderr, and error.",
-      func=self.langchain_call,
-    )
-    tool.args_schema = LangchainCodeInterpreterToolInput
-    return tool
+    # langchain does not return a dict as a parameter, only a code string
+    def langchain_call(self, code: str):
+        return self.call({"code": code})
+
+    def to_langchain_tool(self) -> Tool:
+        tool = Tool(
+            name=self.tool_name,
+            description="Execute python code in a Jupyter notebook cell and returns any rich data (eg charts), stdout, stderr, and error.",
+            func=self.langchain_call,
+        )
+        tool.args_schema = LangchainCodeInterpreterToolInput
+        return tool
+
+    @staticmethod
+    def format_to_tool_message(
+        agent_action: ToolAgentAction,
+        observation: dict,
+    ) -> List[BaseMessage]:
+        new_messages = list(agent_action.message_log)
+        # Here we filter out non-LLM output from CodeInterpreter tool (eg charts)
+        # TODO: How do we nicely give users access to these outputs though?
+        # Users probably need it to render charts, send it to frontend, etc
+
+        content = json.dumps(
+            {k: v for k, v in observation.items() if k not in ("results")}, indent=2
+        )
+        new_messages.append(
+            ToolMessage(content=content, tool_call_id=agent_action.tool_call_id)
+        )
+
+        return new_messages
--- examples/langchain-python/langchain_e2b_python/main.py
+++ examples/langchain-python/langchain_e2b_python/main.py
@@ -1,67 +1,69 @@
 import json
+
 from typing import List, Sequence, Tuple
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 from langchain_e2b_python.code_interpreter_tool import CodeInterpreterFunctionTool
-from langchain.agents import AgentExecutor, create_tool_calling_agent
+from langchain.agents import AgentExecutor
 from langchain_openai import ChatOpenAI
-from langchain.agents.format_scratchpad.tools import (
-    format_to_tool_messages,
-)
-from langchain_core.messages import BaseMessage, ToolMessage
+from langchain_core.messages import BaseMessage
 from langchain_core.runnables import RunnablePassthrough
 from langchain.agents.output_parsers.tools import (
     ToolAgentAction,
     ToolsAgentOutputParser,
 )
 
+
 load_dotenv()
+
 
 def format_to_tool_messages(
     intermediate_steps: Sequence[Tuple[ToolAgentAction, dict]],
 ) -> List[BaseMessage]:
     messages = []
     for agent_action, observation in intermediate_steps:
-        new_messages = list(agent_action.message_log)
-        # Here we filter out non-LLM output from CodeInterpreter tool (eg charts)
-        # TODO: How do we nicely give users access to these outputs though?
-        # Users probably need it to render charts, send it to frontend, etc
+        if agent_action.tool == CodeInterpreterFunctionTool.tool_name:
+            new_messages = CodeInterpreterFunctionTool.format_to_tool_message(
+                agent_action,
+                observation,
+            )
+            messages.extend([new for new in new_messages if new not in messages])
+        else:
+            print("Not handling tool: ", agent_action.tool)
+            # TODO: Handle other tools
+            pass
 
-        content = json.dumps(
-            {k: v for k, v in observation.items() if k not in ("result")}, indent=2
-        )
-        new_messages.append(
-            ToolMessage(content, tool_call_id=agent_action.tool_call_id)
-        )
-        messages.extend([new for new in new_messages if new not in messages])
     return messages
 
 
 def main():
-  # 1. Pick your favorite llm
-  llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
-  # llm = ChatGroq(temperature=0, model_name="llama3-70b-8192")
-  code_interpreter = CodeInterpreterFunctionTool().to_langchain_tool()
-  tools = [code_interpreter]
-  prompt = ChatPromptTemplate.from_messages(
-    [("human", "{input}"), ("placeholder", "{agent_scratchpad}")]
-  )
-
-  agent = (
-    RunnablePassthrough.assign(
-        agent_scratchpad=lambda x: format_to_tool_messages(x["intermediate_steps"])
+    # 1. Pick your favorite llm
+    llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+    # llm = ChatGroq(temperature=0, model_name="llama3-70b-8192")
+    code_interpreter = CodeInterpreterFunctionTool().to_langchain_tool()
+    tools = [code_interpreter]
+    prompt = ChatPromptTemplate.from_messages(
+        [("human", "{input}"), ("placeholder", "{agent_scratchpad}")]
     )
-    | prompt
-    | llm.bind_tools(tools)
-    | ToolsAgentOutputParser()
-  )
 
-  agent_executor = AgentExecutor(
-    agent=agent, tools=tools, verbose=True, return_intermediate_steps=True
-  )
-  result = agent_executor.invoke({"input": "print foo"})
-  print(result)
+    agent = (
+        RunnablePassthrough.assign(
+            agent_scratchpad=lambda x: format_to_tool_messages(x["intermediate_steps"])
+        )
+        | prompt
+        | llm.bind_tools(tools)
+        | ToolsAgentOutputParser()
+    )
 
-  # Each intermediate step is a Tuple[ToolAgentAction, dict]
-  print(result["intermediate_steps"][0][1])
+    agent_executor = AgentExecutor(
+        agent=agent,
+        tools=tools,
+        verbose=True,
+        return_intermediate_steps=True,
+    )
+    result = agent_executor.invoke({"input": "print foo"})
+    print(result)
+
+    # Each intermediate step is a Tuple[ToolAgentAction, dict]
+    print(result["intermediate_steps"][0][1]["results"])

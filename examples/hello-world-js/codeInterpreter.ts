0eb17326eae7cee22a9dee7ce5bd93d7fa88919a
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2024-04-27 00:35:55 -0700

    Remove unused imports

--- examples/hello-world-js/codeInterpreter.ts
+++ examples/hello-world-js/codeInterpreter.ts
@@ -1,5 +1,4 @@
-import { CodeInterpreter, Result, Execution } from '@e2b/code-interpreter'
-
+import { CodeInterpreter } from '@e2b/code-interpreter'
 
 export async function codeInterpret(codeInterpreter: CodeInterpreter, code: string) {
   console.log(`\n${'='.repeat(50)}\n> Running following AI-generated code:\n${code}\n${'='.repeat(50)}`);

60a3dd921807a64a91d153a1b3bd4cb481695ec9
Tereza Tizkova <tereza@e2b.dev>
2024-05-22 13:36:07 +0200

    Update functions for chatting with Claude

--- examples/amazon-bedrock-code-interpreter/app.ts
+++ examples/amazon-bedrock-code-interpreter/app.ts
@@ -10,14 +10,32 @@ import {
 import { CodeInterpreter, Result } from '@e2b/code-interpreter'
 import { ProcessMessage } from '@e2b/code-interpreter'
 import * as dotenv from 'dotenv'
+import { Tool, ToolUseBlock } from '@anthropic-ai/sdk/resources/beta/tools/messages.mjs'
 
 
 // Claude example with Bedrock: https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/javascriptv3/example_code/bedrock-runtime/models/anthropic_claude/claude_3.js
+// Amazon Bedrock Model access: https://docs.aws.amazon.com/bedrock/latest/userguide/model-access.html#model-access-permissions
 
 dotenv.config()
 
-const MODEL_NAME = 'claude-3-opus-20240229'
-const SYSTEM_PROMPT = `
+// const MODEL_NAME = 'claude-3-opus-20240229'
+// const SYSTEM_PROMPT = `
+// ## your job & context
+// you are a python data scientist. you are given tasks to complete and you run python code to solve them.
+// - the python code runs in jupyter notebook.
+// - every time you call \`execute_python\` tool, the python code is executed in a separate cell. it's okay to multiple calls to \`execute_python\`.
+// - display visualizations using matplotlib or any other visualization library directly in the notebook. don't worry about saving the visualizations to a file.
+// - you have access to the internet and can make api requests.
+// - you also have access to the filesystem and can read/write files.
+// - you can install any pip package (if it exists) if you need to but the usual packages for data analysis are already preinstalled.
+// - you can run any python code you want, everything is running in a secure sandbox environment.
+
+// ## style guide
+// tool response values that have text inside "[]"  mean that a visual element got rendered in the notebook. for example:
+// - "[chart]" means that a chart was generated in the notebook.
+// `
+
+    const SYSTEM_PROMPT = `
 ## your job & context
 you are a python data scientist. you are given tasks to complete and you run python code to solve them.
 - the python code runs in jupyter notebook.
@@ -33,6 +51,8 @@ tool response values that have text inside "[]"  mean that a visual element got 
 - "[chart]" means that a chart was generated in the notebook.
 `
 
+
+// Definine tools (TBD: Here or after the model instance is created?)
 const tools: Array<Tool> = [
     {
         name: 'execute_python',
@@ -50,7 +70,7 @@ const tools: Array<Tool> = [
     }
 ]
 
-
+// Define code interpreter function (TBD: Here or after the model instance is created?)
 async function codeInterpret(codeInterpreter: CodeInterpreter, code: string): Promise<Result[]> {
     console.log('Running code interpreter...')
 
@@ -69,50 +89,90 @@ async function codeInterpret(codeInterpreter: CodeInterpreter, code: string): Pr
 }
 
 
-  // Create a new Bedrock Runtime client instance.
-  const client = new BedrockRuntimeClient({ region: "us-east-1" });
+export const invokeModel = async (
+    prompt="Calculate value of pi using monte carlo method. Use 1000 iterations. Visualize all point of all iterations on a single plot, a point inside the unit circle should be orange, other points should be grey.",
+    modelId = "anthropic.claude-3-haiku-20240307-v1:0",
+  ) => {
 
 
-  async function processToolCall(codeInterpreter: CodeInterpreter, toolName: string, toolInput: any): Promise<Result[]> {
-      if (toolName === 'execute_python') {
-          return await codeInterpret(codeInterpreter, toolInput['code'])
-      }
-      return []
-  }
+    // Create a new Bedrock Runtime client instance.
+    const client = new BedrockRuntimeClient({ region: "us-east-1" });
 
-  async function chatWithClaude(codeInterpreter: CodeInterpreter, userMessage: string): Promise<Result[]> {
-      console.log(`\n${'='.repeat(50)}\nUser Message: ${userMessage}\n${'='.repeat(50)}`)
 
-      console.log('Waiting for Claude to respond...')
-      const message = await client.beta.tools.messages.create({
-          model: MODEL_NAME,
-          system: SYSTEM_PROMPT,
-          max_tokens: 4096,
-          messages: [{ role: 'user', content: userMessage }],
-          tools: tools,
-      })
+    // Prepare the payload for the model.
+    const payload = {
+        anthropic_version: "bedrock-2023-05-31",
+        max_tokens: 1000,
+        messages: [
+        {
+            role: "user",
+            content: [{ type: "text", text: prompt }],
+        },
+        ],
+    };
 
-      console.log(`\nInitial Response:\nStop Reason: ${message.stop_reason}`)
+    // Invoke Claude with the payload and wait for the response.
+    const command = new InvokeModelCommand({
+        contentType: "application/json",
+        body: JSON.stringify(payload),
+        modelId,
+    });
+    const apiResponse = await client.send(command);
 
-      if (message.stop_reason === 'tool_use') {
-          const toolUse = message.content.find(block => block.type === 'tool_use') as ToolUseBlock
-          if (!toolUse){
-              console.error('Tool use block not found in message content.')
-              return []
-          }
 
-          const toolName = toolUse.name
-          const toolInput = toolUse.input
+    async function processToolCall(codeInterpreter: CodeInterpreter, toolName: string, toolInput: any): Promise<Result[]> {
+        if (toolName === 'execute_python') {
+            return await codeInterpret(codeInterpreter, toolInput['code'])
+        }
+        return []
+    }
 
-          console.log(`\nTool Used: ${toolName}\nTool Input: ${JSON.stringify(toolInput)}`)
+    async function chatWithClaude(codeInterpreter: CodeInterpreter, userMessage: string): Promise<Result[]> {
+        console.log(`\n${'='.repeat(50)}\nUser Message: ${userMessage}\n${'='.repeat(50)}`)
 
-          const codeInterpreterResults = await processToolCall(codeInterpreter, toolName, toolInput)
-          console.log(`Tool Result: ${codeInterpreterResults}`)
-          return codeInterpreterResults
-      }
-      throw new Error('Tool use block not found in message content.')
-  }
+        console.log('Waiting for Claude to respond...')
+        const message = await client.beta.tools.messages.create({
+            model: modelId,
+            system: `
+            ## your job & context
+            you are a python data scientist. you are given tasks to complete and you run python code to solve them.
+            - the python code runs in jupyter notebook.
+            - every time you call \`execute_python\` tool, the python code is executed in a separate cell. it's okay to multiple calls to \`execute_python\`.
+            - display visualizations using matplotlib or any other visualization library directly in the notebook. don't worry about saving the visualizations to a file.
+            - you have access to the internet and can make api requests.
+            - you also have access to the filesystem and can read/write files.
+            - you can install any pip package (if it exists) if you need to but the usual packages for data analysis are already preinstalled.
+            - you can run any python code you want, everything is running in a secure sandbox environment.
+            
+            ## style guide
+            tool response values that have text inside "[]"  mean that a visual element got rendered in the notebook. for example:
+            - "[chart]" means that a chart was generated in the notebook.
+            `,
+            max_tokens: 4096,
+            messages: [{ role: 'user', content: userMessage }],
+            tools: tools,
+        })
 
+        console.log(`\nInitial Response:\nStop Reason: ${message.stop_reason}`)
+
+        if (message.stop_reason === 'tool_use') {
+            const toolUse = message.content.find(block => block.type === 'tool_use') as ToolUseBlock
+            if (!toolUse){
+                console.error('Tool use block not found in message content.')
+                return []
+            }
+
+            const toolName = toolUse.name
+            const toolInput = toolUse.input
+
+            console.log(`\nTool Used: ${toolName}\nTool Input: ${JSON.stringify(toolInput)}`)
+
+            const codeInterpreterResults = await processToolCall(codeInterpreter, toolName, toolInput)
+            console.log(`Tool Result: ${codeInterpreterResults}`)
+            return codeInterpreterResults
+        }
+        throw new Error('Tool use block not found in message content.')
+    }
 
   async function run() {
       const codeInterpreter = await CodeInterpreter.create()
@@ -132,6 +192,8 @@ async function codeInterpret(codeInterpreter: CodeInterpreter, code: string): Pr
       } finally {
           await codeInterpreter.close()
       }
-  }
+  } 
 
   run()
+
+} // This is closing body of InvokeModel function

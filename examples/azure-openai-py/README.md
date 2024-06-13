8bd2bb670d0a090e2ec59ef87269de0730241ec6
Tereza Tizkova <tereza@e2b.dev>
2024-06-13 15:26:44 +0200

    Modify main program for Azure

--- examples/azure-openai-py/README.md
+++ examples/azure-openai-py/README.md
@@ -1,7 +1,8 @@
 1. Install OpenAI and E2B.
 
 ```
-pip install openai e2b-code-interpreter
+pip install e2b-code-interpreter openai==1.12.0 jupyter requests pytz pandas tenacity azure-search-documents==11.4.0b8
+
 ```
 
 
@@ -27,3 +28,23 @@ Get your API key [here](https://e2b.dev/docs/getting-started/api-key).
 
 
 3. Create a new Python application
+
+
+4. Add functions
+In order to use the OpenAI library or REST API with Microsoft Azure endpoints, you need to set your AZURE_OPENAI_ENDPOINT in the config.json file. We've prepopulated the MODEL_NAME and OPENAI_API_VERSION variables for you in the config.json file with default values. You can change these values if you like.
+
+Add OPENAI_API_KEY as variable name and <Your API Key Value> as variable value in the environment variables.
+
+One can get the OPENAI_API_KEY value from the Azure Portal. Go to https://portal.azure.com, find your resource and then under "Resource Management" -> "Keys and Endpoints" look for one of the "Keys" values.
+
+Great example: https://github.com/Azure-Samples/openai/blob/main/Basic_Samples/Functions/working_with_functions.ipynb
+
+
+Tools (previously called functions) is an optional parameter in the Chat Completion API which can be used to provide function specifications. This allows models to generate function arguments for the specifications provided by the user.
+
+5. Steps for Function Calling with OpenAI:
+
+Call the model with the user query and a set of functions defined in the functions parameter.
+The model can choose to call a function; if so, the content will be a stringified JSON object adhering to your custom schema (note: the model may generate invalid JSON or hallucinate parameters).
+Parse the string into JSON in your code, and call your function with the provided arguments if they exist.
+Call the model again by appending the function response as a new message, and let the model summarize the results back to the user.
--- examples/azure-openai-py/main.py
+++ examples/azure-openai-py/main.py
@@ -1,5 +1,13 @@
 import os
 from openai import AzureOpenAI
+import json # for function calling
+import inspect # for checking correct arguments are provided to function
+
+
+# Load config values
+with open(r"config.json") as config_file:
+    config_details = json.load(config_file)
+
 
 client = AzureOpenAI(
   azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
@@ -7,14 +15,172 @@ client = AzureOpenAI(
   api_version="2024-02-01"
 )
 
-response = client.chat.completions.create(
-    model="gpt-35-turbo", # model = "deployment_name".
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
-        {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
-        {"role": "user", "content": "Do other Azure AI services support this too?"}
-    ]
-)
+model_name = config_details["MODEL_NAME"] # We need to ensure the version of the model you are using supports the function calling feature
 
-print(response.choices[0].message.content)
+# TEST FUNCTIONS
+
+# This code calls the model with the user query and the set of functions defined in the functions parameter.
+# The model then can choose if it calls a function. If a function is called, the content will be in a strigified JSON object.
+# The function call that should be made and arguments are location in: response[choices][0][function_call].
+
+# Define the functions to use
+
+
+def get_function_call(messages, tool_choice="auto"):
+    tools = [
+        {
+            "type": "function",
+            "function": {
+                "name": "execute_python",
+                "description": "Execute python code in a Jupyter notebook cell and returns any result, stdout, stderr, display_data, and error.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "code": {
+                            "type": "string",
+                            "description": "The python code to execute in a single cell.",
+                        },
+                    },
+                    "required": ["code"],
+                },
+            },
+        },
+    ]
+
+
+    # Call the model with the user query (messages) and the functions defined in the functions parameter
+
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=messages,
+        tools=tools,
+        tool_choice=tool_choice,
+    )
+
+    return response
+
+
+# DEFINING FUNCTIONS IN CODE (here just code interpreter)
+
+def code_interpret(e2b_code_interpreter, code):
+  print("Running code interpreter...")
+  exec = e2b_code_interpreter.notebook.exec_cell(
+    code,
+    on_stderr=lambda stderr: print("[Code Interpreter]", stderr),
+    on_stdout=lambda stdout: print("[Code Interpreter]", stdout),
+    # You can also stream code execution results
+    # on_result=...
+  )
+
+  if exec.error:
+    print("[Code Interpreter ERROR]", exec.error)
+  else:
+    return exec.results
+  
+
+# CHECKING THAT CORRECT ARGUMENTS ARE PROVIDED
+
+def check_args(function, args):
+    sig = inspect.signature(function)
+    params = sig.parameters
+
+    # Check if there are extra arguments
+    for name in args:
+        if name not in params:
+            return False
+    # Check if the required arguments are provided
+    for name, param in params.items():
+        if param.default is param.empty and name not in args:
+            return False
+
+    return True
+
+# PUT EVERYTHING TOGETHER 
+# TBD ADD CODE INTERPRETER
+
+def run_conversation(messages, tools, available_functions):
+    # Step 1: send the conversation and available functions to GPT
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=messages,
+        tools=tools,
+        tool_choice="auto",
+    )
+
+    response_message = response.choices[0].message
+
+    # Step 2: check if GPT wanted to call a function
+    if response_message.tool_calls:
+        print("Recommended Function call:")
+        print(response_message.tool_calls[0])
+        print()
+
+        # Step 3: call the function
+        # Note: the JSON response may not always be valid; be sure to handle errors
+
+        function_name = response_message.tool_calls[0].function.name
+
+        # verify function exists
+        if function_name not in available_functions:
+            return "Function " + function_name + " does not exist"
+        function_to_call = available_functions[function_name]
+
+        # verify function has correct number of arguments
+        function_args = json.loads(response_message.tool_calls[0].function.arguments)
+        if check_args(function_to_call, function_args) is False:
+            return "Invalid number of arguments for function: " + function_name
+        function_response = function_to_call(**function_args)
+
+        print("Output of function call:")
+        print(function_response)
+        print()
+
+        # Step 4: send the info on the function call and function response to GPT
+
+        # adding assistant response to messages
+        messages.append(
+            {
+                "role": response_message.role,
+                "function_call": {
+                    "name": response_message.tool_calls[0].function.name,
+                    "arguments": response_message.tool_calls[0].function.arguments,
+                },
+                "content": None,
+            }
+        )
+
+        # adding function response to messages
+        messages.append(
+            {
+                "role": "function",
+                "name": function_name,
+                "content": function_response,
+            }
+        )  # extend conversation with function response
+
+        print("Messages in second request:")
+        for message in messages:
+            print(message)
+        print()
+
+        second_response = client.chat.completions.create(
+            messages=messages,
+            model=model_name,
+        )  # get a new response from GPT where it can see the function response
+
+        return second_response
+
+
+# USING CHAG COMPLETION API BTW
+
+# response = client.chat.completions.create(
+#     model="gpt-35-turbo", # model = "deployment_name".
+#     messages=[
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
+#         {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
+#         {"role": "user", "content": "Do other Azure AI services support this too?"}
+#     ]
+# )
+
+# print(response.choices[0].message.content)

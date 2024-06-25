dc5999cba92f371af2a7ab4fe79bc728efc0a524
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2024-06-25 09:51:26 -0700

    Set up stage 1

--- examples/anthropic-power-artifacts/app/api/chat/route.ts
+++ examples/anthropic-power-artifacts/app/api/chat/route.ts
@@ -11,12 +11,21 @@ import { anthropic } from '@ai-sdk/anthropic'
 import {
   runPython,
 } from '@/lib/sandbox'
+import { prompt } from '@/lib/prompt'
 
 export interface ServerMessage {
   role: 'user' | 'assistant' | 'function';
   content: string;
 }
 
-// export async function POST(req: Request) {
-//   // TODO: Implement
-// }
+export async function POST(req: Request) {
+  const { messages } = await req.json()
+  const result = await streamText({
+    model: anthropic('claude-3-5-sonnet-20240620'),
+    tools: { }, // TODO
+    system: prompt,
+    messages,
+  })
+
+  return result.toAIStreamResponse()
+}

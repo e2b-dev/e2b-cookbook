b77cdf3bb39398cd236aa656a50772038674f296
James Murdza <james@jamesmurdza.com>
2024-09-23 14:46:48 -0700

    Use result_as_answer=True and a more specific prompt to improve reliability

--- examples/crewai-python/crewai_e2b_python/main.py
+++ examples/crewai-python/crewai_e2b_python/main.py
@@ -2,19 +2,14 @@ from dotenv import load_dotenv
 from crewai import Agent, Task  
 from crewai_e2b_python.code_interpreter_tool import CodeInterpreterTool
 import json
-import re
 
 load_dotenv()
-
-# Extract code from an LLM output
-def extract_code(text: str) -> dict:
-    result = re.search(r'```.*?\n(.*?)\n```', text, re.DOTALL)
-    return result.group(1) if result else text
 
 def main():
 
     # Initialize the code interpreter tool
-    code_interpreter = CodeInterpreterTool();
+    # We use result_as_answer=True to prevent the agent from changing the output after the last step
+    code_interpreter = CodeInterpreterTool(result_as_answer=True);
 
     # Create the CrewAI agent
     agent = Agent(
@@ -25,17 +20,24 @@ def main():
         verbose=True,
     )
 
-    # Define the web scraping task
+    # Define the task at hand
+    # We specify that the code should use the print() function rather than leaving the variable on the last line
     scrape_hacker_news = Task(
         description='Scrape the Hacker News homepage.',
-        expected_output='list of articles as a JSON array like [{"headline","url"},...]',
+        expected_output='Print the list of articles as a JSON array like [{"headline","url"},...] using the print() function.',
         agent=agent,
     )
 
-    # Run the agent and print the results
-    task_result = agent.execute_task(scrape_hacker_news)
-    parsed_result = json.loads(extract_code(task_result))
-    print(json.dumps(parsed_result, indent=2))
+    # Run the agent
+    task_output = agent.execute_task(scrape_hacker_news)
+
+    # Parse the last printed line from the code interpreter's output
+    task_result = json.loads(task_output)["stdout"][-1]
+    parsed_result = json.loads(task_result)
+
+    # Print the results
+    BLUE, RESET = '\033[94m', '\033[0m'
+    print(f"{BLUE}{json.dumps(parsed_result, indent=2)}{RESET}")
 
     # Close the code interpreter
     code_interpreter.close()

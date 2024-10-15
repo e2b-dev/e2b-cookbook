740cdb5d7264ab95140858e315926312df754209
James Murdza <james@jamesmurdza.com>
2024-10-15 07:52:57 -0700

    Propagate errors in TypeScript examples

--- examples/together-ai-with-code-interpreting/together-ai-code-interpreter-js/index.ts
+++ examples/together-ai-with-code-interpreting/together-ai-code-interpreter-js/index.ts
@@ -200,6 +200,7 @@ async function run() {
 
     } catch (error) {
         console.error('An error occurred:', error)
+        throw error;
     } finally {
         await codeInterpreter.kill()
     }

a7bc74ae9a4e99fe74d175a9126664b88ade539d
Jakub Dobry <jakub.dobry8@gmail.com>
2025-10-15 00:13:48 +0200

    templates polishing

--- examples/openai-codex-in-sandbox-python/template.py
+++ examples/openai-codex-in-sandbox-python/template.py
@@ -3,26 +3,22 @@ from e2b import Template
 template = (
     Template()
     .from_image("ubuntu:22.04")
-    .set_user("root")
-    .set_workdir("/")
+    # Install Node.js 24.0
+    .apt_install(["curl", "git-all", "ripgrep"])
     .set_envs(
         {
             "DEBIAN_FRONTEND": "noninteractive",
         }
     )
-    # Install Node.js 24.0
-    # update and install curl in a single RUN command to reduce layers
-    .run_cmd("apt-get update && apt-get install -y curl git-all ripgrep")
     # get install script and pass it to execute, then install nodejs in a single RUN command
     .run_cmd(
-        "curl -sL https://deb.nodesource.com/setup_24.x | bash - && apt-get install -y nodejs"
+        "curl -sL https://deb.nodesource.com/setup_24.x | bash - && apt-get install -y nodejs",
+        user="root",
     )
     # confirm that it was successful
     .run_cmd("node -v && npm -v")
     #
     #####
     # Install OpenAI Codex
-    .run_cmd("npm install -g @openai/codex")
-    .set_user("user")
-    .set_workdir("/home/user")
+    .npm_install("@openai/codex")
 )
--- examples/playwright-in-e2b/template/template.ts
+++ examples/playwright-in-e2b/template/template.ts
@@ -5,12 +5,11 @@ export const template = Template()
   .setUser("root")
   .setWorkdir("/")
   // Install Node.js dependencies
-  .runCmd("mkdir /app")
   .setWorkdir("/app")
   // Initialize a new Node.js project
   .runCmd("npm init -y")
   // Install Playwright Node.js package
-  .runCmd("npm install playwright")
+  .npmInstall(["playwright"])
   // Install Playwright browsers and dependencies
   .runCmd(
     "PLAYWRIGHT_BROWSERS_PATH=0 npx playwright install --with-deps chromium",
